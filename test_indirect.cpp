d46f42b14450a982e3161334fa7b9564788cdfb3
Antony Peacock <ant.peacock@gmail.com>
2019-10-17 11:38:39 +0100

    Ensure targets depending on indirect support C++17

--- test_indirect.cpp
+++ test_indirect.cpp
@@ -14,7 +14,7 @@ TEST_CASE("Defeault construction for indirect", "[constructor.default]")
 
 TEST_CASE("Move construction for indirect of a primitive type", "[constructor.move.primitive]")
 {
-    const int a_value = 5;
+    constexpr int a_value = 5;
     indirect<int> a{new int(a_value) }, b{ std::move(a) };
     REQUIRE(*b == a_value); 
     REQUIRE(a.operator->() == nullptr); 
@@ -22,7 +22,7 @@ TEST_CASE("Move construction for indirect of a primitive type", "[constructor.mo
 
 TEST_CASE("Move assignment for indirect of a primative type", "[assignment.move.primitive]")
 {
-    const int a_value = 5, b_value = 10;
+    constexpr int a_value = 5, b_value = 10;
     indirect<int> a{ new int(a_value) }, b{ new int(b_value) };
     a = std::move(b);
     REQUIRE(*a == b_value); 
