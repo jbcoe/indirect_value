76d3fb38df15f81e4f7cf63d1b777e2a77e58da4
Ukilele <kilis-mail@web.de>
2022-02-22 19:36:27 +0100

    Fix copy functions (#71)
    * Copy construct C from i.get_c(). Also copy the deleter.
    
    * In assignment operator, assign the deleter. In case you assign from an empty i.ptr_, also reset this->ptr_

--- indirect_value.h
+++ indirect_value.h
@@ -62,9 +62,9 @@ class indirect_value : private indirect_value_base<T, C> {
 
   explicit indirect_value(const indirect_value& i)
       ISOCPP_P1950_REQUIRES(std::is_copy_constructible_v<T>)
-      : base(get_c()) {
+      : base(i.get_c()) {
     if (i.ptr_) {
-      ptr_ = std::unique_ptr<T, D>(get_c()(*i.ptr_), D{});
+      ptr_ = std::unique_ptr<T, D>(get_c()(*i.ptr_), i.ptr_.get_deleter());
     }
   }
 
@@ -75,11 +75,9 @@ class indirect_value : private indirect_value_base<T, C> {
       ISOCPP_P1950_REQUIRES(std::is_copy_assignable_v<T>) {
     base::operator=(i);
     if (i.ptr_) {
-      if (!ptr_) {
-        ptr_ = std::unique_ptr<T, D>(get_c()(*i.ptr_), D{});
-      } else {
-        ptr_.reset(get_c()(*i.ptr_));
-      }
+      ptr_ = std::unique_ptr<T, D>(get_c()(*i.ptr_), i.ptr_.get_deleter());
+    } else {
+      ptr_.reset();
     }
     return *this;
   }
